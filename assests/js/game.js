3c2df0acc82779470419e333c594ad207daea748
Marcus Hale <marcusxhale1@gmail.com>
2022-01-12 20:21:30 -0800

    game developed, I think what's next is getting something that repeats till the game is over

--- assests/js/game.js
+++ assests/js/game.js
@@ -11,6 +11,30 @@ console.log(enemyName, enemyHealth, enemyAttack);
 
 var fight = function() {
     window.alert("Welcome to the game of life!");
+    enemyHealth = enemyHealth - playerAttack;
+
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    )
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+    playerHealth = playerHealth - enemyAttack;
+
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    if (playerHealth <= 0){
+        window.alert(playerName + "is temporarily out of commission. He'll be back though. I promise.");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left. He's coming back stronger. Best believe.");
+    }
+
+
 };
 
 fight();
