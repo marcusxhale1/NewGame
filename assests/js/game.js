21d5fd7eb46489ceb2d454ceb1389a0e28fff5f0
Marcus Hale <marcusxhale1@gmail.com>
2022-01-12 21:05:19 -0800

    Fight or Sleep?

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your name?");
 var playerHealth = 100; 
 var playerAttack = 10; 
+var playerMoney = 10; 
 
 console.log(playerName, playerAttack, playerHealth);
 
@@ -11,30 +12,41 @@ console.log(enemyName, enemyHealth, enemyAttack);
 
 var fight = function() {
     window.alert("Welcome to the game of life!");
-    enemyHealth = enemyHealth - playerAttack;
+    var promptFight = window.prompt("Would you like to fight and rise up against life or would you like to sleep through life and be a loser?");
+   if (promptFight === "fight" || promptFight === "FIGHT") {
+       enemyHealth = enemyHealth - playerAttack;
+       console.log(
+           playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+       );
 
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    )
-    if (enemyHealth <= 0){
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-    playerHealth = playerHealth - enemyAttack;
+       if (enemyHealth <= 0) {
+           window.alert(enemyName + "is dead, but be weary, he will come back alive in a different form. A form you have never seen.");
+       } else {
+           window.alert(enemyName + " still has " + enemyHealth + " health left. Kill this MF before it kills you.");
+       }
 
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-    if (playerHealth <= 0){
-        window.alert(playerName + "is temporarily out of commission. He'll be back though. I promise.");
-    }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left. He's coming back stronger. Best believe.");
-    }
+       playerHealth = playerHealth - enemyAttack;
+       console.log(
+           enemyName + "attacked " + playerName + "." + playerName + " now has " +playerHealth + "health reamining. What are you going to do? Are you going to stay down and sleep, or are you going to get back up and fight?"
+       );
+       if (playerHealth <= 0) {
+           window.alert(playerName + "is temporarily out of commission, but they'll be back stronger than ever. We don't ever quit!");
 
+       }
+       else {
+           window.alert(playerName + "still has " + enemyHealth + " health left. You need to kill this MF. Come back stratgetically, and stronger!");
+       }
+   }
+   else if (promptFight === "Sleep" || promptFight === "SLEEP"){
+       var confrimSkip = window.confirm("Are you sure you want to skip this specific life battle?");
+       if(confrimSkip) {
+       window.alert(playerName + "has chosen to skip this life battle!");
+       playerMoney = playerMoney - 2;
+       }
 
+   } else{
+       fight();
+   }
 };
 
 fight();
